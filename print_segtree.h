266c20d71bad6a6988030ed1f77330c84d273847
Leonardo <linop3132@gmail.com>
2024-03-20 19:02:07 +0100

    Add guard clause

--- print_segtree.h
+++ print_segtree.h
@@ -1,6 +1,11 @@
+#ifndef PRINT_SEGTREE_H
+#define PRINT_SEGTREE_H
+
 template<typename T>
 class TreePrinter {
 public:
     void print(T t[], int n, int indent);
-    TreePrinter() {}
+    TreePrinter();
 };
+
+#endif
