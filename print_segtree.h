d77aef172b2db860fb7f52286b1eb4e81e3d8e43
Leonardo <linop3132@gmail.com>
2024-03-20 12:09:14 +0100

    Modify print_node function to account for previous node box drawings

--- print_segtree.h
+++ print_segtree.h
@@ -1,23 +1,34 @@
 #include <bits/stdc++.h>
 
 namespace detail {
+
+    enum direction { UP, DOWN };
+
     template<typename T>
-    void print_node(T t[], int v, int spaces, std::string(*func)(T)) {
-        std::cout << " ";
-        for (int i=1; i<spaces; ++i) std::cout << " ";
+    void print_node(T t[], int v, int spaces, int indent, std::string(*func)(T), std::vector<bool>* lines, direction d) {
+        for (int i=0; i<spaces-indent; ++i) {
+            if ((*lines)[i]) std::cout << "|";
+            else std::cout << " ";
+        }
+
+        if (d == UP) std::cout << "\u250C";
+        else /* d == DOWN */ std::cout << "\u2514";
+
+        for (int i=spaces-indent+1; i<spaces; ++i) std::cout << "\u2500";
+
         std::cout << func(t[v]) << std::endl;
     
     }
 
     template<typename T>
-    void print_segtree_aux(T t[], int v, int tl, int tr, int spaces, int indent, std::string(*func)(T)) {
+    void print_segtree_aux(T t[], int v, int tl, int tr, int spaces, int indent, std::string(*func)(T), std::vector<bool>* lines) {
         if (tl == tr) {
-            print_node<T>(t, v, spaces, func);
+            print_node<T>(t, v, spaces, indent, func, lines);
         }
         else {
             int tm = (tl + tr) / 2;
             print_segtree_aux<T>(t, v*2+1, tm+1, tr, spaces+indent, indent, func);
-            print_node<T>(t, v, spaces, func);
+            print_node<T>(t, v, spaces, indent, func, lines);
             print_segtree_aux<T>(t, v*2, tl, tm, spaces+indent, indent, func);
         }
     }
@@ -38,5 +49,5 @@ namespace format {
 
 template<typename T>
 void print_segtree(T t[], int n, int indent) {
-    detail::print_segtree_aux(t, 1, 1, n, 0, indent, format::pii);
+    detail::print_segtree_aux(t, 1, 1, n, 0, indent, format::pii, new std::vector<bool>());
 }
