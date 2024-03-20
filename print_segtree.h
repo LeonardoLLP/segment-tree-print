ff33c2b386cff11bfc53b1480a4d150fa1f1a90a
Leonardo <linop3132@gmail.com>
2024-03-20 05:13:45 +0100

    Changed to single header file Reason: c++ doesn't allow for general templating for functions with header and implementation separation and it wants the developer to explicitely tell what template variations, but I don't know what node structures I will be using.

---
+++ print_segtree.h
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+namespace detail {
+    template<typename T>
+    void print_node(T t[], int v, int spaces, std::string(*func)(T)) {
+        std::cout << ' ';
+        for (int i=0; i<spaces; ++i) std::cout << ' ';
+        std::cout << func(t[v]) << std::endl;
+    
+    }
+
+    template<typename T>
+    void print_segtree_aux(T t[], int v, int tl, int tr, int spaces, int indent, std::string(*func)(T)) {
+        if (tl == tr) {
+            print_node<T>(t, v, spaces, func);
+        }
+        else {
+            int tm = (tl + tr) / 2;
+            print_segtree_aux<T>(t, v*2+1, tm+1, tr, spaces+2, indent, func);
+            print_node<T>(t, v, spaces, func);
+            print_segtree_aux<T>(t, v*2, tl, tm, spaces+2, indent, func);
+        }
+    }
+}
+
+namespace format {
+    std::string pii(std::pair<int,int> x) {
+        return std::to_string(x.first) + " | " + std::to_string(x.second);
+    }
+}
+
+template<typename T>
+void print_segtree(T t[], int n, int indent) {
+    detail::print_segtree_aux(t, 1, 1, n, 0, indent, format::pii);
+}
